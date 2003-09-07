50482101bc5bc94ccc13afe847536156b0dcc5be
Eric Fry <yuv422@users.noreply.github.com>
2003-09-07 13:40:16 +0000

    Fix for stackable items in Event::Get.

--- Event.cpp
+++ Event.cpp
@@ -371,10 +371,18 @@ bool Event::get(sint16 rel_x, sint16 rel_y)
        Actor *pc = player->get_actor();
        float total = (weight*(obj->qty?obj->qty:1)) + pc->get_inventory_weight();
        //float max = pc->inventory_get_max_weight();
+
        if(total <= pc->inventory_get_max_weight())
          {
-           pc->inventory_add_object(obj);
-           obj_manager->remove_obj(obj);
+          obj_manager->remove_obj(obj); //remove object from map.
+
+          if(obj->qty > 0) //stackable object
+            {
+             pc->inventory_add_object(obj->obj_n, obj->qty, obj->quality);
+             delete obj;
+            }
+           else
+             pc->inventory_add_object(obj);
          }
       else
          scroll->display_string("\n\nThe total is too heavy.");
