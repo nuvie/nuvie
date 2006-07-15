5f79fda9ee56cdaf138b61793a9c34eab817af7a
Joseph Applegate <Zabeus@users.noreply.github.com>
2006-07-15 08:53:07 +0000

    include to define ActorList

---
+++ actors/ActorList.h
@@ -0,0 +1,8 @@
+// include this if using ActorList
+#include <vector>
+#include <algorithm>
+
+class Actor;
+
+typedef std::vector<Actor *> ActorList;
+typedef std::vector<Actor *>::iterator ActorIterator;
