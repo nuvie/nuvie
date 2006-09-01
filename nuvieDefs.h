366f9ab53d1522c834e3d362d661c8c519e47b48
Joseph Applegate <Zabeus@users.noreply.github.com>
2006-09-01 04:55:48 +0000

    Tried to clarify how WRAP_COORD works, and make it not rely so much on actual plane number, but it looks even more confusing now.

--- nuvieDefs.h
+++ nuvieDefs.h
@@ -42,8 +42,9 @@ typedef uint8 nuvie_game_t; // Game type (1=u6,2=md,4=se)
 #define clamp_max(v, c)  (((v) > (c)) ? (c) : (v))
 #define clamp(v, c1, c2) ( ((v) < (c1)) ? (c1) : (((v) > (c2)) ? (c2) : (v)) )
 
-#define WRAPPED_COORD(c,level) ((c)&((level)?255:1023))
-#define WRAP_COORD(c,level) ((c)&=((level)?255:1023))
+const uint8 map_pitch[2] = { 1024, 256 }; // width of 0:surface plane, and 1:all other planes // FIXME: U6 only?
+#define WRAPPED_COORD(c,level) ((c)&(map_pitch[(level==0)?0:1]-1)) // mask high bit, wrap C to map_pitch
+#define WRAP_COORD(c,level) ((c)&=(map_pitch[(level==0)?0:1]-1)) // modifies C
 
 #define NUVIE_DIR_N    0
 #define NUVIE_DIR_E    1
