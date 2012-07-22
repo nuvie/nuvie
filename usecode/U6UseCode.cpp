e45c5499472a1f428dc43b357b2f479ff4f2464a
Malignant Manor <malignantmanor@users.noreply.github.com>
2012-07-22 23:01:51 +0000

    Fix forgotten function call

--- usecode/U6UseCode.cpp
+++ usecode/U6UseCode.cpp
@@ -2152,7 +2152,7 @@ bool U6UseCode::use_balloon(Obj *obj, UseCodeEvent ev)
 	 for (sint8 ix=-1;ix<2;ix++) 
 	 {
 	   DEBUG(0,LEVEL_DEBUGGING,"can drop at %d %d?\n",ix,iy); 
-	   if (Game::get_game()->get_map_window()->can_drop_obj(x+ix,y+iy,balloonist)) 
+	   if (Game::get_game()->get_map_window()->can_drop_obj(x+ix,y+iy,balloonist,false)) 
 	   {
 	     DEBUG(0,LEVEL_DEBUGGING,"yes, can drop at %d %d.\n",x+ix,y+iy); 
 	     actor_manager->get_actor_holding_obj(obj)->inventory_remove_obj(obj); // fixme make this work with containers
