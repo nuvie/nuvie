89df7a3c60257f43b08d06563ee0497072c7c4d9
Malignant Manor <malignantmanor@users.noreply.github.com>
2014-03-14 12:31:58 +0000

    * Use isprint instead of isalnum when determining whether to use custom   key bindings in cutscenes and intro menus.

--- script/ScriptCutscene.cpp
+++ script/ScriptCutscene.cpp
@@ -948,7 +948,7 @@ static int nscript_input_poll(lua_State *L)
 			if((((key.mod & KMOD_CAPS) == KMOD_CAPS && (key.mod & KMOD_SHIFT) == 0) || ((key.mod & KMOD_CAPS) == 0 && key.mod & KMOD_SHIFT))
 			   && key.sym >= SDLK_a && key.sym <= SDLK_z)
 				key.sym = (SDLKey)(key.sym -32);
-			if(!isalnum(key.sym) || (key.mod &KMOD_ALT) || (key.mod &KMOD_CTRL)|| (key.mod &KMOD_META))
+			if(!isprint(key.sym) || (key.mod &KMOD_ALT) || (key.mod &KMOD_CTRL)|| (key.mod &KMOD_META))
 			{
 				ActionType a = keybinder->get_ActionType(key);
 				switch(keybinder->GetActionKeyType(a))
